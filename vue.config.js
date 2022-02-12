52977b2b1ed667d3e77a1454211ff1aab44ef736
Skaloshad <dalex90mail@gmail.com>
2022-02-12 16:28:40 +0300

    try make release

---
+++ vue.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    pluginOptions: {
+      electronBuilder: {
+        builderOptions: {
+          win: {
+            icon: 'public/Money_-_Flickr_-_AMagill.png'
+          }
+        }
+      }
+    }
+  }
